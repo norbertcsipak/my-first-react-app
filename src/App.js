773f087af1d56c6d7e6a047068ab44914087b0bb
Norbert Csipak <norbertcipak@me.com>
2022-08-19 14:51:49 +0100

    filtered list after filtering returns to its original starting list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
     console.log('constructor')
   }
@@ -28,6 +29,11 @@ class App extends Component {
 
   render() {
     console.log('render')
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
@@ -35,17 +41,14 @@ class App extends Component {
           type='search'
           placeholder="search monsters"
           onChange={(event) => {
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
+            const searchField = event.target.value.toLocaleLowerCase();
 
             this.setState(() => {
-              return {monsters: filteredMonsters};
+              return { searchField };
             });
           }}/>
 
-          {this.state.monsters.map((monster) => {
+          {filteredMonsters.map((monster) => {
               return <h1 key={monster.id}>{monster.name}</h1>
             })
           }
