d3ada42570f81c218471fb2f2f68ae65815dd882
Norbert Csipak <norbertcipak@me.com>
2022-08-19 13:52:42 +0100

    added search-box, used filter(), includes() to filter the list from searc box as case insensitive

--- src/App.js
+++ src/App.js
@@ -10,28 +10,45 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor')
   }
 
   componentDidMount(){
+    console.log('Mount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then(users => this.setState(() => {
         return {monsters: users}
       },
       () => {
-        console.log(this.state)
+        console.log(this.state);
       }
       ));
   }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <h1 key={monster.id}>{monster.name}</h1>
-          })
-        }
+        <input
+          className='search-box'
+          type='search'
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return {monsters: filteredMonsters};
+            });
+          }}/>
+
+          {this.state.monsters.map((monster) => {
+              return <h1 key={monster.id}>{monster.name}</h1>
+            })
+          }
       </div>
     );
   }
